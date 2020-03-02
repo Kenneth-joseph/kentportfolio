d56fce002fae201b9f600d060af0557fd93c3323
Kenneth-joseph <josephochieng812@gmail.com>
2020-03-02 16:02:51 +0300

    added templates

--- folio/views.py
+++ folio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
+
+class HomePage(ListView):
+    template_name = 'home.html'
